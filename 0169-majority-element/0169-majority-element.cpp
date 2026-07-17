d21fc256c2888bba071799a6c1a598ff80f8f5e0
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-07-18 01:05:28 +0530

    Time: 0 ms (100%), Space: 28.3 MB (35.44%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq=1;
+        int num=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(num==nums[i]){
+                freq++;
+            }
+            else{
+                freq--;
+            }
+            if(freq==0){
+                num=nums[i];
+                freq=1;
+            }
+        }
+    return num;
+    }
+};
