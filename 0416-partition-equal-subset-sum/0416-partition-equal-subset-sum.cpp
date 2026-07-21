77b393581c5522c389950254707a6da677c80bd1
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-07-21 16:55:43 +0530

    Time: 65 ms (87.16%), Space: 15.7 MB (75.14%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int n=nums.size();
-        int target=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-        }
-        if(sum%2==1){
-            return false;
-        }
-        
-        else{
-            target=sum/2;
-        }
-        vector<vector<int>>arr(n,vector<int>(target+1,-1));
-        for(int i=0;i<n;i++){
-            arr[i][0]=1;
-        }
-        for(int i=1;i<target+1;i++){
-            arr[0][i]=0;    
-        }
-        if(target>nums[0]){
-            arr[0][nums[0]]=1;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<target+1;j++){
-                int take=0;
-                if(nums[i]<=j){
-                    take=arr[i-1][j-nums[i]];
-                }
-                int not_take=arr[i-1][j];
+        int sum = 0;
+        int n = nums.size();
 
-                arr[i][j]=take || not_take;
+        for (int x : nums)
+            sum += x;
+
+        if (sum % 2)
+            return false;
+
+        int target = sum / 2;
+
+        vector<int> prev(target + 1, 0), curr(target + 1, 0);
+
+        prev[0] = 1;
+        if (nums[0] <= target)
+            prev[nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            curr[0] = 1;
+
+            for (int j = 1; j <= target; j++) {
+                int take = 0;
+                if (nums[i] <= j)
+                    take = prev[j - nums[i]];
+
+                int not_take = prev[j];
+
+                curr[j] = take || not_take;
             }
+
+            prev = curr;
         }
-    return arr[n-1][target];
+
+        return prev[target];
     }
 };
