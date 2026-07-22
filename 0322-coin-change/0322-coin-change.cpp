5d25eea72495e5018f62f20a818dfbb56c8d224f
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-07-22 19:21:15 +0530

    Time: 26 ms (64.96%), Space: 22 MB (50.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-
-        vector<int> prev(amount + 1, -1), curr(amount + 1, -1);
-
-        prev[0] = 0;
-
-        for (int j = 1; j <= amount; j++) {
-            if (j % coins[0] == 0)
-                prev[j] = j / coins[0];
-        }
-
-        for (int i = 1; i < n; i++) {
-            curr[0] = 0;
-
-            for (int j = 1; j <= amount; j++) {
-                int notTake = prev[j];
-                int take = -1;
-
-                if (j >= coins[i] && curr[j - coins[i]] != -1)
-                    take = curr[j - coins[i]] + 1;
-
-                if (notTake == -1)
-                    curr[j] = take;
-                else if (take == -1)
-                    curr[j] = notTake;
-                else
-                    curr[j] = min(notTake, take);
+        int n=coins.size();
+        vector<int>prev(amount+1,-1);
+        vector<int>curr(amount+1,-1);
+        prev[0]=0;
+        for(int i=1;i<amount+1;i++){
+            if(i%coins[0]==0){
+                prev[i]=i/coins[0];
             }
-
-            prev = curr;
         }
+        for(int i=1;i<n;i++){
+            curr[0]=0;
+            for(int j=1;j<=amount;j++){
+                int nottake=prev[j];
+                int take=-1;
+                if(j>=coins[i]&&curr[j-coins[i]]!=-1){
+                    take=1+(curr[j-coins[i]]);
+                }
+                if(nottake==-1){
+                    curr[j]=take;
+                }
+                else if(take==-1){
+                    curr[j]=nottake;
+                }
+                else{
+                    curr[j]=min(take,nottake);
+                }
+            }
+            prev=curr;
 
-        return prev[amount];
+        }
+    return prev[amount];
     }
+
 };
