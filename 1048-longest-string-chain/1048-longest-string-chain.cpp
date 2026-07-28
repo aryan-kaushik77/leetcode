38176b925e51f2dd6f9edde5573dd279a07cce60
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-07-28 19:53:04 +0530

    Time: 44 ms (59.78%), Space: 18.7 MB (69.23%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPredecessor(string &a, string &b) {
+        if (b.size() != a.size() + 1)
+            return false;
+
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return i == a.size();
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), [](string &a, string &b) {
+            return a.size() < b.size();
+        });
+
+        int n = words.size();
+        vector<int> dp(n, 1);
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (isPredecessor(words[j], words[i])) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+};
