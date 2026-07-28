f61ddfbfd286e1ce403c36200b5f3a04a67b830e
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-07-28 19:57:09 +0530

    Time: 38 ms (78.63%), Space: 18.7 MB (69.23%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -30,8 +30,8 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (isPredecessor(words[j], words[i])) {
-                    dp[i] = max(dp[i], dp[j] + 1);
+                if (isPredecessor(words[j], words[i])&&dp[j]+1>dp[i]) {
+                    dp[i]=dp[j]+1;
                 }
             }
             ans = max(ans, dp[i]);
