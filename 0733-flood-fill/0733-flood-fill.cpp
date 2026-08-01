5e623e5f6cbaf4c32fadd982d037bc1d71cc2e1e
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-08-02 02:07:11 +0530

    Time: 0 ms (100%), Space: 18.3 MB (43.79%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size();
+        int n=image[0].size();
+        queue<pair<int,int>>q;
+        int original=image[sr][sc];
+        if(original==color){
+            return image;
+        }
+        q.push({sr,sc});
+        image[sr][sc]=color;
+        int dr[]={1,-1,0,0};
+        int dc[]={0,0,1,-1};
+        while (!q.empty()){
+            auto [r,c]=q.front();
+            q.pop();
+            for(int k=0;k<4;k++){
+                int nr=r+dr[k];
+                int nc=c+dc[k];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && image[nr][nc]==original){
+                    image[nr][nc]=color;
+                    q.push({nr,nc});
+                }
+            }
+        }
+        return image;
+
+        }
+};
