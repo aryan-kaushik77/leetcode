423817e7f30b7b7af56ae210250b86b089ca78f9
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-08-03 22:40:21 +0530

    Time: 0 ms (100%), Space: 14.5 MB (60.63%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        queue<pair<int,int>> q;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if((i == 0 || j == 0 || i == m-1 || j == n-1) && board[i][j] == 'O') {
+                    q.push({i, j});
+                    board[i][j] = '#';       
+                }
+            }
+        }
+
+        int dr[] = {1, -1, 0, 0};
+        int dc[] = {0, 0, 1, -1};
+
+        // BFS
+        while(!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O') {
+                    board[nr][nc] = '#';
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        // Flip surrounded O's to X and restore boundary-connected O's
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+};
