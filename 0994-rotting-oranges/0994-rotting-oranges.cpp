380fb77dd5e94d9cbea225794d3d3f2c176f3e9c
Aryan Kaushik <aryan24123@iiitd.ac.in>
2026-08-03 17:21:08 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,63 +3,49 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
         int fresh = 0;
         queue<pair<int, int>> q;
-
-        // Count fresh oranges and push all rotten oranges
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
                     fresh++;
-                } else if (grid[i][j] == 2) {
-                    q.push({i, j});
+                }
+                else if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-
-        if (fresh == 0) {
+        if(fresh==0){
             return 0;
         }
-
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        int minutes = 0;
-
-        while (!q.empty()) {
-            bool rotThisTime = false;
-            int size = q.size();
-
-            while (size--) {
-                auto [r, c] = q.front();
+        int minutes=0;
+        int dr[]={1,-1,0,0};
+        int dc[]={0,0,1,-1};
+        while(!q.empty()){
+            bool rot=false;
+            int size=q.size();
+            while(size--){
+                auto [r,c]=q.front();
                 q.pop();
-
-                for (int k = 0; k < 4; k++) {
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-
-                    if (nr >= 0 && nr < n &&
-                        nc >= 0 && nc < m &&
-                        grid[nr][nc] == 1) {
-
-                        grid[nr][nc] = 2;
+                for(int k=0;k<4;k++){
+                    int nr=r+dr[k];
+                    int nc=c+dc[k];
+                    if(nr >=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
                         fresh--;
-                        q.push({nr, nc});
-                        rotThisTime = true;
+                        grid[nr][nc]=2;
+                        q.push({nr,nc});
+                        rot=true;
                     }
                 }
             }
-
-            if (rotThisTime) {
+            if(rot==true){
                 minutes++;
             }
         }
-
-        if (fresh > 0) {
+        if(fresh>0){
             return -1;
         }
-
         return minutes;
+
     }
 };
